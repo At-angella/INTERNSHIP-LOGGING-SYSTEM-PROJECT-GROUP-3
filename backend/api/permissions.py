6b3d3d52f3abf0374dc05504d6c518161e0b5957
Yalo2 <122814840+Yalo2@users.noreply.github.com>
2026-04-12 16:57:08 +0300

    created the permission.py file and added admin permission

---
+++ backend/api/permissions.py
@@ -0,0 +1,22 @@
+from rest_framework.permissions import BasePermission
+
+# Custom permissions for role-based access control.
+
+# BASED ON ROLE
+
+class IsAuthenticated(BasePermission):
+    #Rejects all unauthenticated requests outright.
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated)
+    
+class IsAdmin(BasePermission):
+    """
+    Allows access only to ADMIN users.
+    Used for: registering supervisors, managing placements,
+    approving/rejecting placements, viewing audit logs.
+    """
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated and
+            request.user.role == 'ADMIN'
+        )
