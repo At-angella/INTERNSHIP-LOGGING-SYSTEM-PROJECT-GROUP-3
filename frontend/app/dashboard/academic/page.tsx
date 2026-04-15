7255432ea04a1d0d9d2371206e2e190330bb86ab
mponyemathias <mponyemathias@gmail.com>
2026-04-15 17:33:27 +0300

    make visible to supervisor

--- frontend/app/dashboard/academic/page.tsx
+++ frontend/app/dashboard/academic/page.tsx
@@ -36,7 +36,15 @@ const AcademicSupervisorDashboard =()=>{
   </div>
   <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-8">
   <div classNmae="bg-white p-6 rounded-lg shadow border">
-  <h3 className="text-gray-500 text-sm">Assigned Students</h3></div></div>
+  <h3 className="text-gray-500 text-sm">Assigned Students</h3>
+  <p className="text-3xl font-bold">{students.length}</p>
+  </div>
+ <div className="bg-white p-6 rounded-lg shadow border">
+ <h3 className="text-gray-500 text-sm">Completed assesments</h3>
+ <p className="text-3xl font-bold text-green-600">12</p>
+ </div>
+ </div>
+
   
 }
 const AcademicSupervisorDashboard
