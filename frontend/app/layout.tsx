dd45857a971065b53412d4279977cf68787815b1
At-angella <atangella12@gmail.com>
2026-05-02 20:26:49 +0300

    Created the application main layout

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { AuthProvider } from './lib/auth';
+import './globals.css';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <AuthProvider>
+          {children}
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}
