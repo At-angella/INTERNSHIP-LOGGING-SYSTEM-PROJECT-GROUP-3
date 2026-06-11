68d3d969a161c93190fbcff4c8cdbbddae52646a
Yalo2 <122814840+Yalo2@users.noreply.github.com>
2026-06-11 14:16:09 +0300

    added postgres

--- backend/core/models.py
+++ backend/core/models.py
@@ -10,7 +10,7 @@ import string
 
 # USER MANAGEMENT MODELS
 
-class CustomUserManager(BaseUserManager):\1
+class CustomUserManager(BaseUserManager):
 
     ROLE_EMAIL_DOMAINS = {
         'ADMIN': '@mak.ac.ug',
