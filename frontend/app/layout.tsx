f8a47b30e09b99f42fa8d91b321d1e11636bf251
At-angella <atangella12@gmail.com>
2026-06-13 20:53:22 +0300

    updated ui

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,14 +1,15 @@
 'use client';
 
 import './globals.css';
+import { AuthProvider } from '@/lib/auth';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        
+        <AuthProvider>
           {children}
-        
+        </AuthProvider>
       </body>
     </html>
   );
